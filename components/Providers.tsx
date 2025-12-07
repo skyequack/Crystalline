79ed54fdebc7d6a23f9a5a5f5c56f2594c37a48f
Skye <omer.mitmpl2022@learner.manipal.edu>
2025-12-07 15:05:00 +0530

    providers

--- components/Providers.tsx
+++ components/Providers.tsx
@@ -1,7 +1,7 @@
-'use client';
+"use client";
 
-import { ClerkProvider } from '@clerk/nextjs';
-import { ReactNode } from 'react';
+import { ClerkProvider } from "@clerk/nextjs";
+import { ReactNode } from "react";
 
 export default function Providers({ children }: { children: ReactNode }) {
   return <ClerkProvider>{children}</ClerkProvider>;
